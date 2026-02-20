4a5e6f1cdf9c958f8398088d490e3998a484ebef
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2026-02-20 15:12:49 +0530

    update and edit functionality successfully added

--- todo/script.js
+++ todo/script.js
@@ -1,28 +1,37 @@
 let title = document.getElementById('title');
 let description = document.getElementById('description');
 let element = document.getElementById('items');
-let titles=[]
-let n=0;
-let ele=0;
-const edit =(obj)=>{
-let e = document.getElementsByClassName("adder")[0];
 let updateBtn = document.getElementById('update');
 let editTitle = document.getElementById('editTitle');
 let editDescription =document.getElementById('editDescription');
-updateBtn.addEventListener('click',update(obj));
+let adderBox = document.getElementsByClassName("adder")[0];
+let editorBox = document.getElementsByClassName("editer")[0];
+
+let datas=[]
+let n=0;
+let ele=0;
+
+const edit =(obj)=>{
+updateBtn.addEventListener('click',()=>{update(obj.id)});
 editTitle.value=obj.title;
 editDescription.value=obj.description;
-e.innerHTML=''
-// console.log(obj);
+adderBox.style="display:none;";
+editorBox.style="display:block;"
 }
-const update =(obj)=>{
-console.log(obj);
+
+const update =(id)=>{
+let filtered = datas.filter(n=>n.id!=id);
+filtered.push({id,title:editTitle.value,description:editDescription.value});
+datas=filtered;
+taskUpdater();
+adderBox.style="display:block;";
+editorBox.style="display:none;"
 }
 
 
 const taskUpdater=()=>{
     element.innerHTML='';
-    for(let e of titles)
+    for(let e of datas)
     {
     ele= document.createElement('div');
     ele.id='item';
@@ -37,7 +46,7 @@ const taskUpdater=()=>{
     }
 }
 const add =()=>{
-    titles.push({id:++n,title:title.value,description:description.value});
+    datas.push({id:++n,title:title.value,description:description.value});
     taskUpdater();
     title.value='';
     description.value='';
