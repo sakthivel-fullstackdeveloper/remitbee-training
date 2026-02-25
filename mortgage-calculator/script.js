13754832c9183920e894b60e53545618f7aeeafd
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2026-02-25 17:07:31 +0530

    m300 functionality added , js updated

--- mortgage-calculator/script.js
+++ mortgage-calculator/script.js
@@ -1,10 +1,17 @@
 let element=""
 let ico=""
+let radioValues="";
+
 let obj={rupe:"ico",rupe2:"ico2",rupe3:"ico3"}
+
 const borderClrOn= "hsl(61, 73%, 39%)";
 const bg= "hsl(61, 70%, 52%)";
+
 let radioInterestElement  = document.getElementsByClassName('interest')[0];
 let radioRepaymentElement = document.getElementsByClassName('repayment')[0];
+let amountInput= document.getElementById('rupe');
+let yearsInput = document.getElementById('rupe2');
+let InterestInput = document.getElementById('rupe3');
 
 const turnInterest=()=>{
     radioInterestElement.style=`
@@ -13,6 +20,7 @@ const turnInterest=()=>{
     radioRepaymentElement.style=`
     border: 1px solid #01304c;
     background-color: #e4f4fd;`;
+    radioValues="interest"
 }
 
 const turnRepayment = ()=>{
@@ -22,6 +30,30 @@ const turnRepayment = ()=>{
     radioRepaymentElement.style=`
     border: 1px solid ${borderClrOn};
     background-color: ${bg};`;
+    radioValues="repayment"
+}
+
+const calculate=()=>{
+let amountInputValue= amountInput.value;
+let yearsInputValue = yearsInput.value;
+let InterestInputValue = InterestInput.value;
+
+if(!amountInputValue || !yearsInputValue || !InterestInputValue  || !radioValues){
+    alert("please select all input field");
+    return;
+}
+
+if(radioValues=="interest"){
+   alert(((amountInputValue/100) * (InterestInputValue)) * yearsInputValue);}
+else{
+    alert(
+        (((amountInputValue/100) * (InterestInputValue))* yearsInputValue)+ Number(amountInputValue));
+}
+
+amountInput.value=NaN;
+yearsInput.value=NaN;
+InterestInput.value=NaN;
+
 }
 
 const changer =(ele,icone)=>{
@@ -30,7 +62,6 @@ ele.addEventListener('focus',()=>{
     background-color:${bg};
     border-color:${borderClrOn} !important;
     `});
-
 ele.addEventListener('blur',()=>{icone.style="background-color: #e4f4fd;"})
 }
 
