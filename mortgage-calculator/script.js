87397d11b2c091f34f490b2c1e97277700c6973f
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2026-02-25 15:21:23 +0530

    m300 ui bugs fixed and responsive added

--- mortgage-calculator/script.js
+++ mortgage-calculator/script.js
@@ -1,9 +1,36 @@
 let element=""
 let ico=""
 let obj={rupe:"ico",rupe2:"ico2",rupe3:"ico3"}
+const borderClrOn= "hsl(61, 73%, 39%)";
+const bg= "hsl(61, 70%, 52%)";
+let radioInterestElement  = document.getElementsByClassName('interest')[0];
+let radioRepaymentElement = document.getElementsByClassName('repayment')[0];
+
+const turnInterest=()=>{
+    radioInterestElement.style=`
+    border: 1px solid ${borderClrOn};
+    background-color: ${bg};`
+    radioRepaymentElement.style=`
+    border: 1px solid #01304c;
+    background-color: #e4f4fd;`;
+}
+
+const turnRepayment = ()=>{
+    radioInterestElement.style=`
+     border: 1px solid #01304c;
+    background-color: #e4f4fd;`;
+    radioRepaymentElement.style=`
+    border: 1px solid ${borderClrOn};
+    background-color: ${bg};`;
+}
 
 const changer =(ele,icone)=>{
-ele.addEventListener('focus',()=>{icone.style="background-color: hsl(61, 70%, 52%);"});
+ele.addEventListener('focus',()=>{
+    icone.style=`
+    background-color:${bg};
+    border-color:${borderClrOn} !important;
+    `});
+
 ele.addEventListener('blur',()=>{icone.style="background-color: #e4f4fd;"})
 }
 
@@ -13,3 +40,5 @@ ico = document.getElementById(obj[i]);
 console.log(i,obj[i])
 changer(element,ico);
 }
+
+
