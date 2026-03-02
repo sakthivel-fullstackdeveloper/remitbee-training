2682bb2447d149a4a5c50e95d991b8576110de20
sakthivel-fullstackdeveloper <sakthivel2004it@gmail.com>
2026-03-02 16:15:12 +0530

    m500 100% mortgage calculator completed and dev test done push to production

--- mortgage-calculator/script.js
+++ mortgage-calculator/script.js
@@ -1,8 +1,12 @@
 let element=""
 let ico=""
 let radioValues="";
-
+let value = 0;
+let resulted = document.getElementsByClassName('resulted')[0];
+let started = document.getElementsByClassName('started')[0];
 let obj={rupe:"ico",rupe2:"ico2",rupe3:"ico3"}
+started.style="display:flex;"
+resulted.style="display:none;"
 
 const borderClrOn= "hsl(61, 73%, 39%)";
 const bg= "hsl(61, 70%, 52%)";
@@ -12,6 +16,9 @@ let radioRepaymentElement = document.getElementsByClassName('repayment')[0];
 let amountInput= document.getElementById('rupe');
 let yearsInput = document.getElementById('rupe2');
 let InterestInput = document.getElementById('rupe3');
+let total = document.getElementById('total');
+let num = document.getElementById('num');
+
 
 const turnInterest=()=>{
     radioInterestElement.style=`
@@ -37,6 +44,7 @@ const calculate=()=>{
 let amountInputValue= amountInput.value;
 let yearsInputValue = yearsInput.value;
 let InterestInputValue = InterestInput.value;
+let interest = (amountInputValue /100) * InterestInputValue;
 
 if(!amountInputValue || !yearsInputValue || !InterestInputValue  || !radioValues){
     alert("please select all input field");
@@ -44,22 +52,26 @@ if(!amountInputValue || !yearsInputValue || !InterestInputValue  || !radioValues
 }
 
 if(radioValues=="interest"){
-   alert(((amountInputValue/100) * (InterestInputValue)) * yearsInputValue);}
+   value= (interest/ (Number(yearsInputValue) * 12)).toFixed(2);
+}
 else{
-    alert(
-        (((amountInputValue/100) * (InterestInputValue))* yearsInputValue)+ Number(amountInputValue));
+    value=((Number(amountInputValue))/(Number(yearsInputValue)*12)).toFixed(2);
 }
 
-amountInput.value=NaN;
-yearsInput.value=NaN;
-InterestInput.value=NaN;
+total.innerText = Number(amountInputValue)+interest;
+started.style="display:none;"
+resulted.style="display:flex;"
+num.innerText=value;
+amountInput.value='';
+yearsInput.value='';
+InterestInput.value='';
 
 }
 
 const clearAll=()=>{
-amountInput.value=NaN;
-yearsInput.value=NaN;
-InterestInput.value=NaN;
+amountInput.value='';
+yearsInput.value='';
+InterestInput.value='';
 radioInterestElement.style=`
     border: 1px solid #01304c;
     background-color: #e4f4fd;`;
@@ -69,6 +81,8 @@ radioRepaymentElement.style=`
 document.getElementById('interest').checked=false;
 document.getElementById('repayment').checked=false;
 radioValues="";
+started.style="display:flex;"
+resulted.style="display:none;"
 
 }
 
@@ -87,5 +101,7 @@ ico = document.getElementById(obj[i]);
 console.log(i,obj[i])
 changer(element,ico);
 }
+console.log(started);
+console.log(resulted);
 
 
